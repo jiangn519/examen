ac2e7c5a8101e7323e6c50ad08373f6d2bf1a6fc
lin <lin@DESKTOP-774SKVG>
2019-08-11 23:21:38 -0400

    mavenprojet tester examen

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Employee.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author lin
+ */
+public class Employee {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    
+}
